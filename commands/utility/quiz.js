5a4cb2e3e2945e0426d405ce524552c8a5c73f15
Pandora1337 <dima.t.02@bk.ru>
2021-07-18 23:28:20 +0300

    NEW MENU

--- commands/utility/quiz.js
+++ commands/utility/quiz.js
@@ -1,41 +1,48 @@
-const fs = require('fs');
-const {prefix} = require('../../config.json')
-const { Collection, MessageEmbed } = require('discord.js');
+const { MessageMenuOption, MessageMenu } = require('discord-buttons');
 
 module.exports = {
     name: 'quiz',
     description: 'Lets you play a few quizes!',
     aliases: ['q'],
-    usage: '[quiz name]',
-    example: 'monke',
+    usage: '<quiz name>',
+    emoji: '🤔',
     args: false,
     guildOnly: false,
     masterOnly: false,
-    execute(message, args, client){
-        client.quizes = new Collection();
+    execute(message, args, client) {
 
-        const Folder = fs.readdirSync('./storage/quiz/')
+        //message.delete();
 
-        for (const file of Folder) {
-            if (file.endsWith('.js')){
-            const quiz = require(`../../storage/quiz/${file}`);
-            client.quizes.set(quiz.name, quiz);
-            }
-        }
-        
-        const files2 = client.quizes.filter(quiz => quiz.isQuiz === true)
-            .map(quiz =>  /*${quiz.name.split('.').slice(0,-1)}*/`\`${quiz.name}\` - ${quiz.description} by ${quiz.author}`)
-        // const list2 = 'Here are the quizes you can play:\n\n' + files2.join('\n') + `\n\nProper usage: \`${prefix}q [quiz name]\``
+        const quizmenu = new MessageMenu()
+            .setID('quizmenu')
+            .setPlaceholder('Select a quiz to play!')
+            .setMaxValues(1);
 
-        const embed = new MessageEmbed()
-            .setColor('#35b1c9')
-            .setTitle('Here are the quizzes you can play:')
-            .setDescription(files2.join('\n'))
-            //.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-            .setFooter(`Proper usage:  ${prefix}q [quiz name]`);
-        
-        if (!args.length) { return message.channel.send(embed) }
+        const files3 = client.quizes.filter(quiz => quiz.isQuiz === true);
+        files3.forEach(c => {
 
-        if (client.quizes.some(a => a.name === args[0] == true)) return client.quizes.get(args[0]).execute(message)
+            let option = new MessageMenuOption()
+                .setLabel(c.longName)
+                .setValue(c.name)
+                .setDescription(`${c.description}`);
+            if (c.emoji) { option.setEmoji(c.emoji) }
+            if (c.author) { option.setDescription(`${c.description} by ${c.author}`) }
+
+            quizmenu.addOptions(option)
+
+        });
+
+        if (client.quizes.some(a => a.name === args[0] == true && a.isQuiz == true)) return client.quizes.get(args[0]).execute(message);
+        else {var msg = message.channel.send('Select the quiz you want to play from the menu!\n', quizmenu);}
+
+        client.on('clickMenu', async (menu) => {
+            if (menu.id != 'quizmenu') return;
+
+            try {
+                //message.channel.send(menu.clicker.user.displayAvatarURL({ format: 'jpg' }))
+                await menu.reply.defer();
+                await client.quizes.get(menu.values[0]).execute(message, menu.clicker.user)
+            } catch (err) { }//console.log(err) }//
+        })
     }
 }
