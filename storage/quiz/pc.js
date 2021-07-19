d7724bfc8af1cb249d96bff4ab9cd05f320a4707
Pandora1337 <dima.t.02@bk.ru>
2021-07-19 15:49:48 +0300

    Update pc.js

--- storage/quiz/pc.js
+++ storage/quiz/pc.js
@@ -90,14 +90,15 @@ module.exports = {
             const collector = msg.createReactionCollector(filter, { max: 1, time: 60000*5, errors: ['time'] })
 
             collector.on('collect', async (reaction, user) => {
+                
+                if (message.channel.type !== 'dm') { reaction.users.remove(user.id) }
+                
                 if (reaction.emoji.name === EmojiArray[0]) { next_question(1.0) }
                 if (reaction.emoji.name === EmojiArray[1]) { next_question(0.5) }
                 if (reaction.emoji.name === EmojiArray[2]) { next_question(0.0) }
                 if (reaction.emoji.name === EmojiArray[3]) { next_question(-0.5) }
                 if (reaction.emoji.name === EmojiArray[4]) { next_question(-1.0) }
                 if (reaction.emoji.name === EmojiArray[5]) { prev_question() }
-                
-                if (message.channel.type !== 'dm') { reaction.users.remove(user.id) }
             });
 
             collector.on('end', (collection, reason) => {
