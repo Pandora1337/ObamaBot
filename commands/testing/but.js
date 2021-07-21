c2d7c6ede88cacba9f4fda45001f794657973495
Pandora1337 <dima.t.02@bk.ru>
2021-07-22 02:25:19 +0300

    Update but.js

--- commands/testing/but.js
+++ commands/testing/but.js
@@ -1,4 +1,5 @@
 const disbut = require("discord-buttons");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: 'but',
@@ -10,28 +11,70 @@ module.exports = {
     guildOnly: false,
     permissions: '',
     masterOnly: true,
-    execute(message, args, client){
+    execute(message, args, client) {
 
-    let button = new disbut.MessageButton()
-        .setLabel("This is a button!")
-        .setID("myid")
-        .setStyle("blurple");
+        let button1 = new disbut.MessageButton()
+            .setLabel("Strongly agree")
+            .setID("1")
+            .setStyle("green");
 
+        let button2 = new disbut.MessageButton()
+            .setLabel(" agree")
+            .setID("0.5")
+            .setStyle("green");
 
-    let option = new disbut.MessageMenuOption()
-        .setLabel('Your Label')
-        .setEmoji('🍔')
-        .setValue('menuid')
-        .setDescription('Custom Description!')
+        let button3 = new disbut.MessageButton()
+            .setLabel("neutral")
+            .setID("0")
+            .setStyle("grey");
+
+        let button4 = new disbut.MessageButton()
+            .setLabel("disagree")
+            .setID("-0.5")
+            .setStyle("red");
+
+        let button5 = new disbut.MessageButton()
+            .setLabel("Strongly disagree")
+            .setID("-1")
+            .setStyle("red");
+
+        let button6 = new disbut.MessageButton()
+            .setLabel("fam go bac")
+            .setID("bacc")
+            .setStyle("blurple");
         
-    let select = new disbut.MessageMenu()
-        .setID('customid')
-        .setPlaceholder('Click me! :D')
-        .setMaxValues(1)
-        .setMinValues(1)
-        .addOption(option)
+            
+        let bb = new disbut.MessageButton()
+        .setLabel("yah")
+        .setID("bacc2")
+        .setStyle("green");
 
-        message.channel.send(`bruh`, button);
+
+
+        let row = new disbut.MessageActionRow()
+            .addComponents(button1, button2, button3, button4, button5);
+        let row2 = new disbut.MessageActionRow()
+            .addComponent(button6);
+        
+
+        let embed = new MessageEmbed()
+            .setDescription(' bruh ');
+
+        client.on('clickButton', async (button) => {
+
+            if (button.clicker.user.id != message.author.id) return button.reply.think(true)
+            if (button.id === 'bacc') return await button.message.edit('a', null)
+
+            const math = button.id * 10;
+            embed.setDescription(`${button.id} is invoked by ${button.clicker.user.username}. math: ${math}`)
+
+            await button.message.edit(embed)
+            await button.reply.defer()
+
+        });
+
+
+        message.channel.send(embed, {components: [row, row2]} );
 
     }
 }
