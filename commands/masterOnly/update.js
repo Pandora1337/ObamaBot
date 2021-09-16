a10fb3710cb813019d1d6b56e599f9a5bd2eaa51
Pandora1337 <dima.t.02@bk.ru>
2021-09-17 00:22:23 +0300

    better control over deployment

--- commands/masterOnly/update.js
+++ commands/masterOnly/update.js
@@ -10,7 +10,6 @@ module.exports = {
 	masterOnly: true,
 
 	execute(interaction, args, client) {
-		
 		const guildId = interaction.guildId
 
 		const slCommands = []
@@ -34,6 +33,7 @@ module.exports = {
 
 		const rest = new REST({ version: '9' }).setToken(TOKEN);
 
+		// registering global commands
 		if (args == 'global') {
 
 		(async () => {
@@ -42,7 +42,7 @@ module.exports = {
 					Routes.applicationCommands(botId),
 					{ body: slCommands },
 				);
-				interaction.reply({ content: 'Successfully registered application commands.' })
+				interaction.reply({ content: 'Successfully registered global application commands.' })
 				console.log('Successfully registered global application commands.');
 			} catch (error) {
 				console.error(error);
@@ -50,7 +50,44 @@ module.exports = {
 		})();
 			return
 		}
+		
+		// de-registering commands
+		if (args == 'delete') {
 
+			(async () => {
+				try {
+					await rest.put(
+						Routes.applicationGuildCommands(botId, guildId),
+						{ body: {} }
+					);
+					interaction.reply({ content: 'Successfully de-registered application commands.' })
+					console.log('Successfully de-registered application commands.');
+				} catch (error) {
+					console.error(error);
+				}
+			})();
+				return
+			}
+			
+		// de-registering global commands
+		if (args == 'delete global') {
+
+			(async () => {
+				try {
+					await rest.put(
+						Routes.applicationCommands(botId),
+						{ body: {} }
+					);
+					interaction.reply({ content: 'Successfully de-registered global application commands.' })
+					console.log('Successfully de-registered global application commands.');
+				} catch (error) {
+					console.error(error);
+				}
+			})();
+				return
+			}
+
+			//if guildId is specified
 		if (!isNaN(args) && args.length) {
 
 		(async () => {
@@ -68,6 +105,7 @@ module.exports = {
 			return
 		}
 
+		//standart behaviour
 		(async () => {
 			try {
 				await rest.put(
