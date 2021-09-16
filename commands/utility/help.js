6e48cc33c73c909e0dcf839efd98090234a0aaab
Pandora1337 <dima.t.02@bk.ru>
2021-09-16 22:30:58 +0300

    command '/' update

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,89 +1,106 @@
 const { prefix } = require('../../config.json');
 const logger = require('../../logger.js');
-const { MessageEmbed } = require('discord.js')
-const { MessageMenuOption, MessageMenu, MenuCollector } = require('discord-buttons');
+const { MessageEmbed, MessageSelectMenu, MessageActionRow } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'help',
-    description: `Lists all my commands with a cool new menu!`,
+    description: `Lists all my commands!`,
     aliases: ['h', 'list', 'commands'],
     emoji: '❔',
     usage: '<optional command name>',
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription(`Lists all my commands!`)
+        .addStringOption(option => option.setName('command').setDescription('Command to get more info on')),
+
     async execute(message, args, client) {
 
         const { commands } = message.client;
 
-        if (!args.length) {
-            const files2 = commands.filter(command => command.masterOnly === false || !command.masterOnly)
-
-            const files = commands.filter(command => command.masterOnly === false || !command.masterOnly)
-                //.filter(command => authorPerms.has(command.permissions) || authorPerms)
-                .map(command => `${command.emoji} \`${command.name}\` - ${command.description}`)
-
-            var embedList = new MessageEmbed()
-                .setColor('#FF7700 ')
-                .setTitle('Here\'s a list of my commands:')
-                .setDescription(`${files.join('\n\n')}`)
-                .setThumbnail('https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778')
-                .setFooter(`btw, my prefix is ${prefix}`);
-            //  .setFooter(`You can send  ${prefix}help  [command name]  to get info on a specific command!`);
-
-            var select = new MessageMenu()
-                .setID('helpmenu')
-                .setPlaceholder('Select a command for more info on it!')
-
-            files2.forEach(c => {
-
-                let option = new MessageMenuOption()
-                    .setLabel(c.name)
-                    .setValue(c.name)
-                if (c.emoji) { option.setEmoji(c.emoji) }
-                //.setDescription(element.description);
-
-                select.addOptions(option)
-            });
-
-            var msg = await message.author.send(embedList, select) //message.author.send(data, { split: true })
-
-            if (message.channel.type !== 'dm') {
-                message.reply('I\'ve sent you a DM with my commands!')
-                    .catch(error => {
-                        logger.error(`Could not send help DM.\n`, error);
-                        message.reply('It seems like I can\'t DM you! Do you have DMs disabled?');
-                    });
-            }
+        if (message.type == 'APPLICATION_COMMAND') {  //conversion between interaction and message syntax
+            message.author = message.user
+            if (message.options.getString('command')) { args[0] = message.options.getString('command'); }
         }
 
-        if (args.length) {
+
+        if (args[0]) {
             let data = []
             if (args[0] == 'master') {
                 data.push(`Master-only commands: \n`)
                 data.push(commands.filter(command => command.masterOnly === true)
                     .map(command => `\`` + command.name + `\`` + ` - ` + command.description).join('\n'));
-                return message.author.send(data, { split: true })
+                return message.author.send({ content: data.toString() })
             }
 
             const name = args[0].toLowerCase();
             const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-            if (!command) { return message.reply('That\'s not a valid command!') }
-            return message.channel.send(getHelp(name))
+            if (!command) {
+                return message.reply({ content: 'That\'s not a valid command!', ephemeral: true })
+            }
+            return message.reply({ embeds: [getHelp(name)], allowedMentions: { repliedUser: false }, ephemeral: true })
+        }
+        
+
+        const files2 = commands.filter(command => command.masterOnly === false || !command.masterOnly)
+
+        const files = commands.filter(command => command.masterOnly === false || !command.masterOnly)
+            //.filter(command => authorPerms.has(command.permissions) || authorPerms)
+            .map(command => {
+                const map = ` \`${command.name}\` - ${command.description}`
+                if (command.emoji) return command.emoji + map
+                return map
+            })
+
+        var embedList = new MessageEmbed()
+            .setColor('#FF7700 ')
+            .setTitle('Here\'s a list of my commands:')
+            .setDescription(`${files.join('\n\n')}`)
+            .setThumbnail('https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778')
+            .setFooter(`btw, my prefix is ${prefix}`);
+        //  .setFooter(`You can send  ${prefix}help  [command name]  to get info on a specific command!`);
+
+        var select = new MessageSelectMenu()
+            .setCustomId('helpmenu')
+            .setPlaceholder('Select a command for more info on it!')
+
+        files2.forEach(c => {
+            select.addOptions([
+                {
+                    label: c.name,
+                    value: c.name,
+                    emoji: c.emoji,
+                }
+            ])
+        });
+
+        const row = new MessageActionRow()
+            .addComponents(select)
+
+        message.author.send({ embeds: [embedList], components: [row] }) //message.author.send(data, { split: true })
+
+        if (message.channel.type !== 'DM') {
+            message.reply({ content: 'I\'ve sent you a DM with my commands!', ephemeral: true })
+                .catch(error => {
+                    logger.error(`Could not send help DM.\n`, error);
+                    message.reply({ content: 'It seems like I can\'t DM you! Do you have DMs disabled?', ephemeral: true });
+                });
         }
 
 
-        const filter = (b) => b.clicker.user.bot == false
-
-        var collector = await msg.createMenuCollector(filter, { idle: 60000 * 10, errors: ['time'] })
+        /*
+        var collector = await msg.createMessageComponentCollector({ componentType: 'SELECT_MENU', idle: 60000 * 10, errors: ['time'] })
 
         collector.on('collect', async (menu) => {
-            await menu.reply.send(getHelp(menu.values[0]))
+            await menu.reply({ embeds: [getHelp(menu.values[0])] })
         });
 
         collector.on('end', (menu, reason) => {
             if (reason != 'idle') return
-            msg.edit(embedList, null)
+            msg.edit({ embeds: [embedList], components: [] })
         })
-
+        */
 
         function getHelp(name) {
 
@@ -95,11 +112,12 @@ module.exports = {
             if (command.aliases) data.push(`**Short Name(s):** ${command.aliases.join(', ')}\n`);
             if (command.description) data.push(`**Description:** ${command.description}\n`);
             if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\`\n`);
+            if (command.usageInt) data.push(`**Usage:** ${command.usageInt}\n`);
             if (command.example) data.push(`**Example:** \`${prefix}${command.name} ${command.example}\`\n`);
 
             const helpCommand = new MessageEmbed()
                 .setColor('#ebc83d')
-                .setDescription(data)
+                .setDescription(data.join("\n").toString())
                 .setTitle(`< ${command.name} >`);
             if (command.emoji) { helpCommand.setTitle(`< ${command.emoji} ${command.name} >`) }
 
--- commands/utility/quiz.js
+++ commands/utility/quiz.js
@@ -1,4 +1,5 @@
-const { MessageMenuOption, MessageMenu } = require('discord-buttons');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageSelectMenu, MessageActionRow } = require('discord.js');
 
 module.exports = {
     name: 'quiz',
@@ -9,46 +10,75 @@ module.exports = {
     args: false,
     guildOnly: false,
     masterOnly: false,
-    async execute(message, args, client) {
+    data: new SlashCommandBuilder()
+        .setName('quiz')
+        .setDescription('Lets you play a few quizes!')
+        .addStringOption(option => option.setName('quiz-name').setDescription('The name of a quiz to play')),
 
-        //message.delete();
+    async execute(message, brug, client) {
 
-        const quizmenu = new MessageMenu()
-            .setID('quizmenu')
-            .setPlaceholder('Select a quiz to play!')
-            .setMaxValues(1);
+        if (message.type == 'APPLICATION_COMMAND') { // slash command conversion
+            message.author = message.user
+            var args = message.options.getString('quiz-name')
+            
+        } else if (message.type == 'MESSAGE_COMPONENT') { 
+            message.author = message.user
+            var args = brug
 
-        const files3 = client.quizes.filter(quiz => quiz.isQuiz === true);
+        } else { var args = brug.join(' ').toLowerCase() }
 
-        files3.forEach(c => {
+        
+        if (args != null && client.quizes.some(a => a.longName == args || a.name == args  && a.isQuiz == true)) {
+            const q = client.quizes.find(a => a.longName == args || a.name == args && a.isQuiz == true)
+            //message.reply({ content: `Starting \`${q.longName}\` quiz...`, allowedMentions: { repliedUser: false } });
+            return q.execute(message, message.author);
+        }
 
-            let option = new MessageMenuOption()
-                .setLabel(c.longName)
-                .setValue(c.name)
-                .setDescription(`${c.description}`);
-            if (c.emoji) { option.setEmoji(c.emoji) }
-            if (c.author) { option.setDescription(`${c.description} by ${c.author}`) }
-
-            quizmenu.addOptions(option)
-
+        var msg = await message.reply({
+            content: 'Select the quiz you want to play from the menu!\n',
+            components: [BuildAMenu()],
+            ephemeral: false,
+            fetchReply: true,
+            allowedMentions: { repliedUser: false }
         });
 
-        if (client.quizes.some(a => a.name === args[0] == true && a.isQuiz == true)) return client.quizes.get(args[0]).execute(message, message.author);
-        else {var msg = await message.channel.send('Select the quiz you want to play from the menu!\n', quizmenu);}
-
-
-        const filter = (b) => b.clicker.user.bot == false
-
-        var collector = msg.createMenuCollector(filter, { idle: 60000 * 10, errors: ['time'] })
+/*
+        var collector = msg.createMessageComponentCollector({ componentType: 'SELECT_MENU', idle: 60000 * 10, errors: ['idle'] })
 
         collector.on('collect', async (menu) => {
-            await menu.reply.defer();
-            client.quizes.get(menu.values[0]).execute(message, menu.clicker.user)
+            await menu.deferUpdate();
+            client.quizes.get(menu.values[0]).execute(message, menu.user)
         });
 
         collector.on('end', (menu, reason) => {
             if (reason != 'idle') return
-            msg.edit('Use my \`quiz\` command to play a quiz!', null)
+            msg.edit({ content: 'Use my \`quiz\` command to play a quiz!', components: [] })
         })
+    */
+
+        function BuildAMenu() {
+
+            const quizmenu = new MessageSelectMenu()
+                .setCustomId('quizmenu')
+                .setPlaceholder('Select a quiz to play!')
+                .setMaxValues(1);
+
+            const files3 = client.quizes.filter(quiz => quiz.isQuiz === true);
+
+            files3.forEach(c => {
+                var longDesc = c.description;
+                if (c.author) { var longDesc = `${c.description} by ${c.author}` }
+
+                quizmenu.addOptions([
+                    {
+                        label: c.longName,
+                        description: longDesc,
+                        value: c.name,
+                        emoji: c.emoji,
+                    }
+                ])
+            });
+            return new MessageActionRow().addComponents(quizmenu)
+        }
     }
 }
