73000b8f80f5b216afe0c9991dde67eb5582763f
Pandora1337 <dima.t.02@bk.ru>
2021-12-21 02:42:12 +0300

    invite > support due to "invite bot" button

--- commands/z misc/support.js
+++ commands/z misc/support.js
@@ -2,10 +2,10 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 const { botId } = require('../../config.json')
 
 module.exports = {
-    name: 'invite',
-    description: 'The invites to Obama and his support server. Use this to update bot permissions.',
+    name: 'support',
+    description: 'The invite to Obama\'s support server',
     usage: ' ',
-    aliases: ['support', 'invite'],
+    aliases: ['support'],
     example: '',
     emoji: '🙋',
     args: false,
@@ -14,12 +14,11 @@ module.exports = {
     masterOnly: false,
     execute(message, args, client) {
 
-        const INVITE = `https://discord.com/oauth2/authorize?client_id=${botId}&permissions=2159406144&scope=bot%20applications.commands`;
+        //const INVITE = `https://discord.com/oauth2/authorize?client_id=${botId}&permissions=2159406144&scope=bot%20applications.commands`;
         const SERVER = 'https://discord.com/invite/aaDWzVHS7k';
 
         const embed = new MessageEmbed()
-            .setDescription(`Make sure all my permissions are granted with my [INVITE](${INVITE})`
-                + `\n\nFor support or suggestions, join my [SERVER](${SERVER})`)
+            .setDescription(`For support or suggestions, join my [SERVER](${SERVER})`)
             .setColor('00FFFF')
         //.setTimestamp('2009-01-20T14:17:00.000Z');
         //.setAuthor(client.user.username, client.user.displayAvatarURL())
@@ -27,10 +26,12 @@ module.exports = {
 
         let row = new MessageActionRow()
             .addComponents(
+                /*
                 new MessageButton()
                     .setLabel("INVITE")
                     .setURL(INVITE)
                     .setStyle("LINK"),
+                    */
 
                 new MessageButton()
                     .setLabel("SERVER")
