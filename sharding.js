a565a28a8679d55d3a0de852ae63a3798a4411e8
Pandora1337 <dima.t.02@bk.ru>
2022-02-12 17:21:08 +0300

    removed Sharding due to lowered expectations

--- sharding.js
+++
@@ -1,9 +0,0 @@
-const { TOKEN } = require('./config.json');
-const { ShardingManager } = require('discord.js');
-const logger = require('../logger');
-
-const manager = new ShardingManager('./bot.js', { token: `${TOKEN}`, totalShards: 'auto' });
-manager.on('launch', shard => logger.server(`Launched shard ${shard.id}`));
-manager.spawn();
-
-//  manager.spawn({ amount: 'auto', delay: 5500, timeout: 30000 });
